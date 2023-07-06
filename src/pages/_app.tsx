dff57d0bba31ddb0a6aa4f9df8c984313c35193a
thev3ritas <81315628+mikhailcodes@users.noreply.github.com>
2023-07-06 06:51:11 -0500

    ...

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import '~/styles/global.css'
-
-import { IBM_Plex_Mono, Inter, PT_Serif } from '@next/font/google'
 import type { AppProps } from 'next/app'
 import { lazy } from 'react'
 
+import { NhostProvider, NhostClient, NhostSession } from '@nhost/nextjs';
+
+const nhost = new NhostClient({
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || '',
+  region: process.env.NEXT_PUBLIC_NHOST_REGION || ''
+});
+
 export interface SharedPageProps {
+  nhostSession: NhostSession;
   draftMode: boolean
   token: string
 }
@@ -13,13 +19,10 @@ const PreviewProvider = lazy(() => import('~/components/PreviewProvider'))
 
 
 
-export default function App({
-  Component,
-  pageProps,
-}: AppProps<SharedPageProps>) {
+export default function App({ Component, pageProps, }: AppProps<SharedPageProps>) {
   const { draftMode, token } = pageProps
   return (
-    <>
+    <NhostProvider nhost={nhost} initial={pageProps.nhostSession}>
       {draftMode ? (
         <PreviewProvider token={token}>
           <Component {...pageProps} />
@@ -27,6 +30,6 @@ export default function App({
       ) : (
         <Component {...pageProps} />
       )}
-    </>
+    </NhostProvider>
   )
 }
