bd809a7f1e74a9a7cffbc5829b3b634b8c04d16c
thev3ritas <81315628+mikhailcodes@users.noreply.github.com>
2023-07-17 11:13:47 -0500

    ..

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,17 +1,12 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-import NextLogo from './nextjs.svg'
-import SanityLogo from './sanity.svg'
 
 export default function Welcome() {
   return (
     <div className="welcome__container">
       <div className="logos">
         <div className="logos__blur"></div>
-        <Image className="logos__entry" src={NextLogo} alt="Next.js Logo" />
-        <span className="logos__plus">+</span>
-        <Image className="logos__entry" src={SanityLogo} alt="Sanity Logo" />
       </div>
       <div className="steps">
         <h2 className="steps__title text-2xl">Next steps</h2>
